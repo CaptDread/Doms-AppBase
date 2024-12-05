a3e7b23f042cb81907c49edc2f4c0464652c1aa4
CaptDread <domlycaon@gmail.com>
2024-12-04 19:11:28 -0500

    Not many hours are left in a day, between work and feline care

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import DatsMyLogo from "./scenes/boot/brand";
+import GameBoot from "./scenes/boot/intro";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        
+        {/* <DatsMyLogo/> */}
+        <GameBoot/>
       </main>
     </div>
   );
---
+++ app/scenes/boot/brand.js
@@ -0,0 +1,13 @@
+import styles from "../../page.module.css";
+
+export default function DatsMyLogo(){
+    return(
+        <>
+            <section className={styles.brandShow}>
+                <figure>
+                    <img src={"./../../captainsLogo.png"}/>
+                </figure>
+            </section >
+        </>
+    )
+}
