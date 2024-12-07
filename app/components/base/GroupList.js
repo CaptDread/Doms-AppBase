d84aec8492c0e2fec3f47ac06f289dc000eb681a
CaptDread <domlycaon@gmail.com>
2024-12-06 20:00:52 -0500

    Started GroupList Component

--- app/components/base/GroupList.js
+++ app/components/base/GroupList.js
@@ -1,1 +1,21 @@
 // This component handles listing Todo Lists, yeah plural, as in users can use have more than one todo list.  
+
+const groups = () => localStorage.getItem('groups');
+
+
+
+export default function GetLists(){
+    if (groups.length < 1){
+        console.log("No Groups")
+        return(
+            <>
+                <section>
+                    <h5>You don't have any List, Let's make some!</h5>
+                    <button>Create New List!</button>
+                </section>
+            </>
+        )
+    } else {
+    console.log(groups)
+    }
+}
